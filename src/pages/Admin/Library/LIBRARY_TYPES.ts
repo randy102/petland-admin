ef398e170da3fa4ffe3e89dd2b075f0beb57075f
randy <randy@test-cube.com>
2020-12-14 10:37:28 +0700

    Added Exp type library

--- src/pages/Admin/Library/LIBRARY_TYPES.ts
+++ src/pages/Admin/Library/LIBRARY_TYPES.ts
@@ -1,7 +1,9 @@
 export const LIBRARY_TYPES = [
   {_id: 'qhse', name: 'QHSE'},
+  {_id: 'experience', name: 'Experience'}
 ]
 
 export const LIBRARY_TYPE_GRID: any = {
   qhse: {color: 'blue', name: 'QHSE'},
+  experience: {color: 'green', name: 'Experience'}
 }
