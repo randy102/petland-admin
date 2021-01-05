a0f510e219f7d04d0341fd04357ba1bda5c8803e
randy <randy@test-cube.com>
2021-01-05 20:59:02 +0700

    Fix: filter nested field

--- src/pages/Admin/Project/Grid.tsx
+++ src/pages/Admin/Project/Grid.tsx
@@ -104,8 +104,7 @@ export default function Grid(props: GridProps) {
           },
           {
             title: "Category",
-            dataIndex: "category",
-            render: (category) => category && category[lang],
+            dataIndex: ["category",lang]
           },
           {
             title: "Status",
