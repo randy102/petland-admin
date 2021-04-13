9af9550f234e65aaf28ef1e9bd4927bdc1385c2d
Bùi Trung Bảo <buitrbao222@gmail.com>
2021-04-13 21:11:16 +0700

    Remove create post

--- src/pages/Admin/Post/index.tsx
+++ src/pages/Admin/Post/index.tsx
@@ -1,9 +1,8 @@
+import { UnorderedListOutlined } from '@ant-design/icons';
 import { Tabs } from 'antd';
 import React, { useState } from 'react';
-import { PlusOutlined, UnorderedListOutlined } from '@ant-design/icons';
-import Grid from './Grid';
-import Create from './Create';
 import { useFetch } from 'utils/request';
+import Grid from './Grid';
 
 export default function Post() {
   const [curTab, setCurTab] = useState('list');
@@ -26,18 +25,6 @@ export default function Post() {
           }
         >
           <Grid res={res} loading={loading} refetch={refetch} />
-        </Tabs.TabPane>
-
-        <Tabs.TabPane
-          key="add"
-          tab={
-            <span>
-              <PlusOutlined />
-              Tạo mới
-            </span>
-          }
-        >
-          <Create refetch={refetch} setCurTab={setCurTab} />
         </Tabs.TabPane>
       </Tabs>
     </div>
